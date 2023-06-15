9133dbc345fdd15369fcc5b7fb3eab51f396105b
robertomahl <robertomahl@live.com>
2023-06-15 15:49:32 -0300

    Add map FABs

--- jardim-botanico/src/components/LeafletMap.vue
+++ jardim-botanico/src/components/LeafletMap.vue
@@ -1,35 +1,59 @@
 <template>
   <div id="map"></div>
+
+  <q-page-sticky position="top-right" :offset="[20, 20]">
+    <q-btn fab icon="ion-leaf" color="green" @click="handleClick2()" />
+  </q-page-sticky>
+  <q-page-sticky position="top-right" :offset="[20, 90]">
+    <q-btn fab icon="ion-home" color="red" @click="handleClick2()" />
+  </q-page-sticky>
+  <q-page-sticky position="top-right" :offset="[20, 160]">
+    <q-btn fab icon="ion-flower" color="orange" @click="handleClick2()" />
+  </q-page-sticky>
+
+  <q-page-sticky position="bottom-left" :offset="[20, 20]">
+    <q-btn fab icon="ion-walk" color="grey" @click="handleClick2()" />
+  </q-page-sticky>
+  <q-page-sticky position="bottom-right" :offset="[20, 20]">
+    <q-btn fab icon="ion-camera" color="grey" @click="handleClick2()" />
+  </q-page-sticky>
 </template>
 
 <script>
 import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { defineComponent } from 'vue';
 
-export default {
+export default defineComponent({
   mounted() {
-    this.$nextTick(() => {
-      this.createMap();
-    });
+    this.createMap();
   },
   methods: {
     createMap() {
-      const map = L.map('map').setView([51.505, -0.09], 13);
+      var map = L.map("map", {
+        center: [-29.7194, -53.7295],
+        zoom: 17,
+        zoomControl: false
+      });
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        maxZoom: 18,
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
       }).addTo(map);
-
-      // Add other map-related code here, such as markers, popups, etc.
     },
+
+    handleClick2() {
+      alert('Button 2 clicked');
+    },
+
   },
-};
+});
 </script>
 
 <style>
 #map {
-  height: 100vh;
+  height: calc(100vh - 50px);
   width: 100%;
+  z-index: -1;
 }
 </style>
--- jardim-botanico/src/pages/IndexPage.vue
+++ jardim-botanico/src/pages/IndexPage.vue
@@ -1,20 +1,12 @@
 <template>
   <q-page class="flex flex-center">
-    <!--
-    TODO:
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-    -->
     <LeafletMap></LeafletMap>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import LeafletMap from 'components/LeafletMap.vue'
+import LeafletMap from 'src/components/LeafletMap.vue';
 
 export default defineComponent({
   name: 'IndexPage',
