a6d235d857749d454bf2a79b2ed2f2138d221683
Roberto Mahl <robertomahl@live.com>
2023-10-31 00:02:38 -0300

    Remove extra dependencies

--- jardim-botanico/quasar.config.js
+++ jardim-botanico/quasar.config.js
@@ -45,8 +45,6 @@ module.exports = configure(function (ctx) {
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'ionicons-v4',
-      'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
